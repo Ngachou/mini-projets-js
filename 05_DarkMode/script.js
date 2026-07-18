45e5cf3c6ae2e5db42d91fab366deb037fe65501
Ulrich Ngachou <ulrichlandry722@gmail.com>
2026-07-18 16:49:36 +0100

    add logic of darkMode

--- 05_DarkMode/script.js
+++ 05_DarkMode/script.js
@@ -1,1 +1,8 @@
+const btn = document.getElementById("toggleBtn");
+const html = document.documentElement;
 
+btn.addEventListener("click", () => {
+  const currenttheme = html.getAttribute("data-theme");
+  const newTheme = currenttheme === "dark" ? "light" : "dark";
+  html.setAttribute("data-theme", newTheme);
+});
