035f6cdf27b5a8a6014dc3a96220900252e2d90e
Ulrich Ngachou <ulrichlandry722@gmail.com>
2026-07-22 09:26:29 +0100

    add of the localstorage

--- 05_DarkMode/script.js
+++ 05_DarkMode/script.js
@@ -1,8 +1,10 @@
 const btn = document.getElementById("toggleBtn");
 const html = document.documentElement;
-
+const currenttheme = localStorage.getItem("mode") || "light";
+html.setAttribute("data-theme", currenttheme);
 btn.addEventListener("click", () => {
   const currenttheme = html.getAttribute("data-theme");
   const newTheme = currenttheme === "dark" ? "light" : "dark";
   html.setAttribute("data-theme", newTheme);
+  localStorage.setItem("mode", newTheme);
 });
